1cc396f2b10d0281591b56aa66a7d5e748d4d0a3
Татьяна Павликова <tatv.pav@gmail.com>
2022-05-06 18:48:41 +0300

    Objects commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,65 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  }
+  else {
+    this.marks.push(mark);
+  }  
+}
+
+Student.prototype.addMarks = function (...tmarks) {
+
+  if (this.marks === undefined) {
+
+    this.marks = tmarks;
+
+  }  
+  else {
+
+    /*for (let item of tmarks) {
+
+      this.marks.push(item);
+    }*/
+
+    tmarks.forEach((item, idx, arr) => this.marks.push(item));
+  }  
+   
+}
+
+Student.prototype.getAverage = function () {
+  
+  if (this.hasOwnProperty("marks")) {
+    
+    /*let total = 0, avg;
+    for (let item of this.marks) {
+    
+    total += item;
+  }
+  return avg = total/this.marks.length;*/
+
+    return this.marks.reduce((acc, item, idx, arr) => {
+      if (idx === arr.length-1) {
+        return (acc + item)/arr.length;
+      } else {
+        return acc + item;
+      } 
+    },0);
+  }
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+}
+
