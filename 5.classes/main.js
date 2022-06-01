4514f99a543cf089163aa270051361cf7309e926
Татьяна Павликова <tatv.pav@gmail.com>
2022-06-01 23:21:12 +0300

    Работа по коассам

--- 5.classes/main.js
+++ 5.classes/main.js
@@ -25,3 +25,5 @@ console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
 console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
 library.giveBookByName("Машина времени");
 console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
+
+const student = new Student("Петров Вася");
---
+++ 5.classes/task.js
@@ -0,0 +1,183 @@
+class PrintEditionItem {
+
+	constructor (name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this.state = 100;
+	  this.type = null;
+	}
+
+	fix() {
+		this.state = this.state*1.5;
+	} 
+
+	set state(value) {
+		if (value < 0) {
+			this._state = 0;
+		} else if (value > 100) {
+			this._state = 100;
+		} else {
+			this._state = value;
+		}
+	}
+
+	get state() {
+		return this._state;
+	}   
+}	
+
+class Magazine extends PrintEditionItem {
+	constructor (name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = 'magazine';
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor (author, name, releaseDate, pagesCount) {
+		super(name,releaseDate,pagesCount) 
+		this.author = author;
+		this.type = "book";
+	}
+}
+ class NovelBook extends Book {
+ 	constructor (author, name, releaseDate, pagesCount) {
+ 		super(author,name,releaseDate,pagesCount);
+ 		this.type = "novel";
+ 	}
+ }
+ class FantasticBook extends Book {
+ 	constructor (author, name, releaseDate, pagesCount) {
+ 		super(author,name,releaseDate,pagesCount);
+ 		this.type = "fantastic";
+ 	}
+ }
+ class DetectiveBook extends Book {
+ 	constructor (author, name, releaseDate, pagesCount) {
+ 		super(author,name,releaseDate,pagesCount);
+ 		this.type = "detective";
+ 	}
+ }
+
+ class Library {
+ 	constructor(name) {
+ 		this.name = name;
+ 		this.books = [];
+ 	}
+
+ 	addBook(book) {
+ 		if (book.state > 30) {
+ 			this.books.push(book);
+ 		}
+ 	}
+
+ 	findBookBy(type, value) {
+ 	
+ 		if (this.books.every((item) => item[type] !== value)) {
+ 			return null;
+ 		}
+
+    return this.books.find((item) => item[type] === value);
+       
+    }
+
+    giveBookByName(bookName) {
+
+    	if (this.books.findIndex((item) => item.name === bookName) === -1) {
+    		return null;
+    	}
+
+    	return this.books.splice(this.books.findIndex((item) => {
+    		if (item.name === bookName) {
+    			console.log(item.name);
+    		}}), 1);   	
+
+    }
+}
+
+
+class Journal {
+	constructor (subjectName, marks) {
+		this.subjectName = subjectName;
+		this.marks = marks;
+	}
+}
+
+class Student {
+  constructor (name) {
+  	this.name = name;
+  	this.subjects = []; 
+  }
+
+  addSubject(value) {
+  	this.subjects.push(value);
+  }
+
+  addMark(mark, subjectName) {
+  	if (this.subjects.some((item) => item.subjectName === subjectName) === false) {
+  		    return this.addSubject(new Journal(subjectName, [mark])); 
+  	}
+  	if (mark < 1 || mark > 5) {
+  				return "Ошибка, оценка должна быть числом от 1 до 5";
+  	}
+
+  	this.subjects.find((item) => {
+  		if (item.subjectName === subjectName) {
+  			item.marks.push(mark);
+  		} 
+    });
+  }	
+
+  getAverageBySubject(subjectName) {
+  	let avgSub;
+
+  	if (this.subjects.some((item) => item.subjectName === subjectName) === false) {
+  		return "Несуществующий предмет"
+  	}
+
+  	this.subjects.find((item) => {
+  		if (item.subjectName === subjectName) {
+
+  			return avgSub = item.marks.reduce((acc, item, idx, arr) => {
+
+      		if (idx === arr.length-1) {
+        		return (acc + item)/arr.length;
+      		} 
+      
+      		return acc + item;
+      
+    		},0);   
+  		} 
+    });
+    return avgSub; //`Средний балл по предмету ${subjectName} ${avgSub}`;
+  }
+
+  getAverage() {
+  	let avgTotal, arrMarks = [];
+  	this.subjects.forEach((item) => {
+      item.marks.forEach((item) => {
+      	arrMarks.push(item);
+      });
+
+  	});
+  	//console.log(arrMarks);
+
+  	avgTotal = arrMarks.reduce((acc, item, idx, arr) => {
+
+      		if (idx === arr.length-1) {
+        		return (acc + item)/arr.length;
+      		} 
+      
+      		return acc + item;
+      
+    		},0); 
+    return avgTotal; //`Средний балл по всем предметам ${avgTotal}`;		  
+  }
+
+  exclude(reason) {
+  	delete this.subjects;
+  	this.excluded = reason;
+  }
+}
+
