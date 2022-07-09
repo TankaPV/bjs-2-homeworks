452e8635d19cd7572307ea3b8e35fd5462c7a20a
Татьяна Павликова <tatv.pav@gmail.com>
2022-07-09 15:33:56 +0300

    first commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,80 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    const hash = args.join(',');
+    const objectInCache = cache.find((item) => {
+      if (item.hash === hash) {
+        return item;
+      }
+    }); 
+
+    if (objectInCache) {
+        console.log('Из кэша:' + objectInCache.value);
+        return 'Из кэша: ' + objectInCache.value;
+    }
+
+    if (!objectInCache) {
+      let result = func(...args);
+      cache.push({hash:hash, value: result});
+       
+      if (cache.length > 5) {
+          cache.shift();
+      }   
+
+      console.log('Вычисляем: ' + result);
+      return 'Вычисляем: ' + result;  
+
+    }
+    
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let isThrottled = false;
+  
+  return function(...args) {
+    
+    if (isThrottled) {
+      console.log('Сигнал проигнорирован')
+      return;
+    }
+
+    func.apply(this, args);
+    isThrottled = true;
+    
+    setTimeout(() => {
+      isThrottled = false;
+      func.apply(this, args);
+    }, ms);
+  }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let isThrottled = false;
+
+  wrapper.count = 0;
+
+      function wrapper(...args) {
+
+      wrapper.count += 1;
+      console.log(wrapper.count);
+
+      if (isThrottled) {
+       console.log('Сигнал проигнорирован')
+        return;
+      }
+
+      func.apply(this, args);
+      isThrottled = true;
+      
+
+      setTimeout(() => {
+        isThrottled = false;
+        func.apply(this, args);
+      }, ms);
+    }
+
+    return wrapper;
 }
